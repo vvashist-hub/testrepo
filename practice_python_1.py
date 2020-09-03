371251e418d832eb1766615439014825437c7083
vvashist-hub <70687658+vvashist-hub@users.noreply.github.com>
2020-09-03 15:00:19 +0530

    Add files via upload

---
+++ practice_python_1.py
@@ -0,0 +1,14 @@
+name = input("Your name please:")
+age = int(input("your current age please:"))
+CY = 2020
+Y_hun = 2020 + 100 - age
+
+s = int(input("How time you want to see:"))
+
+print(s)
+
+i = 0
+while (i<s):
+    print("Hello " + name + "! you will reach at 100 in year "+ str(Y_hun))
+    i = i+1
+    
